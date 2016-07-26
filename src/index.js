2b734d8ef6075017746bffeb71e2151013898e6a
Etienne Lemay <etienne@heliom.ca>
2016-07-26 14:53:53 -0400

    Export frequently util

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import emojiIndex from './utils/emoji-index'
 import store from './utils/store'
+import frequently from './utils/frequently'
 
 export { Picker, Emoji } from './components'
-export { emojiIndex, store }
+export { emojiIndex, store, frequently }
