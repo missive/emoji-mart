e8b6e27d33fd09b373568b09641f18bc3e47ae61
Frugality <court@idstrom.com>
2017-01-24 16:58:18 -0800

    Category: export from index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import emojiIndex from './utils/emoji-index'
 import store from './utils/store'
 import frequently from './utils/frequently'
 
-export { Picker, Emoji } from './components'
+export { Picker, Emoji, Category } from './components'
 export { emojiIndex, store, frequently }
