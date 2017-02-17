44d401ae489009b48df3594c22655b175fc93a42
Etienne Lemay <etiennelemay21@gmail.com>
2017-02-17 12:21:04 -0500

    Fix `watch` script not bundling example It also recompiles EmojiMart lib

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 
-var config = require('../src/webpack.config.js'),
+var config = require('../example/webpack.config.js'),
     compiler = webpack(config)
 
 compiler.watch({}, (err, stats) => {
