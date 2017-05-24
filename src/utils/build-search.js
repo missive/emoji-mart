d9c0139d73e71f01a152dcb4fe6d2536c02eeb44
Jakob Krigovsky <jakob@krigovsky.com>
2017-05-24 18:34:10 +0200

    Split out generation of search property

---
+++ src/utils/build-search.js
@@ -0,0 +1,22 @@
+module.exports = data => {
+  const search = []
+
+  var addToSearch = (strings, split) => {
+    (Array.isArray(strings) ? strings : [strings]).forEach((string) => {
+      (split ? string.split(/[-|_|\s]+/) : [string]).forEach((s) => {
+        s = s.toLowerCase()
+
+        if (search.indexOf(s) == -1) {
+          search.push(s)
+        }
+      })
+    })
+  }
+
+  addToSearch(data.short_names, true)
+  addToSearch(data.name, true)
+  addToSearch(data.keywords, false)
+  addToSearch(data.emoticons, false)
+
+  return search
+}
