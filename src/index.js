5c07fdedcd99139ffa9e6ffab5a6f0711db59d78
Etienne Lemay <etienne@heliom.ca>
2018-04-29 11:41:06 -0400

    Export NimblePicker, NimbleEmoji and NimbleCategory

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import emojiIndex from './utils/emoji-index/emoji-index'
 import store from './utils/store'
 import frequently from './utils/frequently'
 
-export { Picker, Emoji, Category } from './components'
+export {
+  Picker,
+  NimblePicker,
+  Emoji,
+  NimbleEmoji,
+  Category,
+  NimbleCategory,
+} from './components'
+
 export { NimbleEmojiIndex } from './utils/emoji-index/nimble-emoji-index'
 export { emojiIndex, store, frequently }
