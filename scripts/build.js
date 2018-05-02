741bf81bb85f578824b06a1791b358a96cd6c6a5
Peder Johnsen <arnjeir+github@gmail.com>
2018-05-02 10:22:45 +0100

    Update build script to add localImages

--- scripts/build.js
+++ scripts/build.js
@@ -49,11 +49,23 @@ module.exports = (options) => {
 
     if (options.sets) {
       var keepEmoji = false
+      datum.localImages = {};
 
       options.sets.forEach((set) => {
         if (keepEmoji) return
-        if (datum[`has_img_${set}`]) {
+
+        var key = `has_img_${set}`
+        if (datum[key]) {
+          datum.localImages[set] = [`require('../emojis/img-${set}-64/${datum.image}')`]
           keepEmoji = true
+
+          // Skin variations
+          if (datum.skin_variations) {
+            for (let skinKey in datum.skin_variations) {
+              var skinVariations = datum.skin_variations[skinKey]
+              if (skinVariations[key]) datum.localImages[set].push(`require('../emojis/img-${set}-64/${skinVariations.image}')`)
+            }
+          }
         }
       })
 
