4f8b29547f1676ac40d0277157256622941f4f33
Peder Johnsen <arnjeir+github@gmail.com>
2018-05-10 14:41:01 +0100

    Make backdrop pressable to close picker

--- src/components/picker/nimble-picker.js
+++ src/components/picker/nimble-picker.js
@@ -50,7 +50,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    zIndex: 2,
     position: 'absolute',
     top: 0,
     right: 0,
@@ -58,6 +57,7 @@ const styles = StyleSheet.create({
     left: 0,
   },
   emojiMartPicker: {
+    zIndex: 2,
     flexShrink: 0,
     flexDirection: 'column',
     backgroundColor: '#fff',
