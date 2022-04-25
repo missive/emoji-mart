e20c71f634ec308cf6361b5971d9f185d38c3a50
Etienne Lemay <etienne@heliom.ca>
2022-04-25 14:45:21 -0400

    Export FrequentlyUsed

--- packages/emoji-mart/src/index.js
+++ packages/emoji-mart/src/index.js
@@ -1,6 +1,6 @@
 export { PickerElement as Picker } from '@components/Picker'
 export { EmojiElement as Emoji } from '@components/Emoji'
 
-export { SearchIndex } from '@helpers'
+export { SearchIndex, FrequentlyUsed } from '@helpers'
 
 export { init, Data, I18n } from '@config'
