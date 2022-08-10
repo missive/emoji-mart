afe2ab499350cb1f5dd79bf22de3a30d5513d7a0
Josh Whitham <jwhitham@digitalstack.io>
2022-08-10 18:24:24 +1000

    Change import to address known issue with @parcel/transformer-typescript-types
    https://github.com/parcel-bundler/parcel/issues/5911

--- packages/emoji-mart/src/components/Picker/index.ts
+++ packages/emoji-mart/src/components/Picker/index.ts
@@ -1,3 +1,3 @@
 export { default as Picker } from './Picker'
 export { default as PickerElement } from './PickerElement'
-export * as PickerStyles from 'bundle-text:./PickerStyles.scss'
+export { default as PickerStyles } from 'bundle-text:./PickerStyles.scss'
