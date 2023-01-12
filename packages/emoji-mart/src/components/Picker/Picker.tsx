missive/emoji-mart

commit 03960bd7371d7ac8d4b9eb8c774f9a1c5dd53228

Add `dynamicWidth` props to Picker

packages/emoji-mart/src/components/Picker/Picker.tsx
@@ -18,8 +18,11 @@ export default class Picker extends Component {
   constructor(props) {
     super()
 
+    this.observers = []
+
     this.state = {
       pos: [-1, -1],
+      perLine: this.initDynamicPerLine(props),
       visibleRows: { 0: true },
       ...this.getInitialState(props),
     }
@@ -33,7 +36,6 @@ export default class Picker extends Component {
   }
 
   componentWillMount() {
-    this.observers = []
     this.dir = I18n.rtl ? 'rtl' : 'ltr'
     this.refs = {
       menu: createRef(),
@@ -133,10 +135,17 @@ export default class Picker extends Component {
     this.observeRows()
   }
 
-  unobserve() {
+  unobserve({ except = [] } = {}) {
+    if (!Array.isArray(except)) {
+      except = [except]
+    }
+
     for (const observer of this.observers) {
+      if (except.includes(observer)) continue
       observer.disconnect()
     }
+
+    this.observers = [].concat(except)
   }
 
   initGrid() {
@@ -173,7 +182,7 @@ export default class Picker extends Component {
       let row = addRow(rows, category)
 
       for (let emoji of category.emojis) {
-        if (row.length == this.props.perLine) {
+        if (row.length == this.getPerLine()) {
           row = addRow(rows, category)
         }
 
@@ -233,6 +242,36 @@ export default class Picker extends Component {
 
       this.closeSkins()
     }
+  }
+
+  initDynamicPerLine(props = this.props) {
+    if (!props.dynamicWidth) return
+    const { element, emojiButtonSize } = props
+
+    const calculatePerLine = () => {
+      const { width } = element.getBoundingClientRect()
+      return Math.floor(width / emojiButtonSize)
+    }
+
+    const observer = new ResizeObserver(() => {
+      this.unobserve({ except: observer })
+      this.setState({ perLine: calculatePerLine() }, () => {
+        this.initGrid()
+        this.forceUpdate(() => {
+          this.observeCategories()
+          this.observeRows()
+        })
+      })
+    })
+
+    observer.observe(element)
+    this.observers.push(observer)
+
+    return calculatePerLine()
+  }
+
+  getPerLine() {
+    return this.state.perLine || this.props.perLine
   }
 
   getEmojiByPos([p1, p2]) {
@@ -349,7 +388,7 @@ export default class Picker extends Component {
     let row = null
 
     for (let emoji of searchResults) {
-      if (!grid.length || row.length == this.props.perLine) {
+      if (!grid.length || row.length == this.getPerLine()) {
         row = []
         row.__categoryId = 'search'
         row.__index = grid.length
@@ -852,6 +891,7 @@ export default class Picker extends Component {
   renderCategories() {
     const { categories } = Data
     const hidden = !!this.state.searchResults
+    const perLine = this.getPerLine()
 
     return (
       <div
@@ -889,9 +929,13 @@ export default class Picker extends Component {
                     return null
                   }
 
-                  const start = i * this.props.perLine
-                  const end = start + this.props.perLine
+                  const start = i * perLine
+                  const end = start + perLine
                   const emojiIds = category.emojis.slice(start, end)
+
+                  if (emojiIds.length < perLine) {
+                    emojiIds.push(...new Array(perLine - emojiIds.length))
+                  }
 
                   return (
                     <div
@@ -903,6 +947,17 @@ export default class Picker extends Component {
                     >
                       {visible &&
                         emojiIds.map((emojiId, ii) => {
+                          if (!emojiId) {
+                            return (
+                              <div
+                                style={{
+                                  width: this.props.emojiButtonSize,
+                                  height: this.props.emojiButtonSize,
+                                }}
+                              ></div>
+                            )
+                          }
+
                           const emoji = SearchIndex.get(emojiId)
 
                           return this.renderEmojiButton(emoji, {
@@ -1048,9 +1103,6 @@ export default class Picker extends Component {
       <section
         id="root"
         class="flex flex-column"
-        style={{
-          width: this.props.perLine * this.props.emojiButtonSize + (12 + 16),
-        }}
         data-emoji-set={this.props.set}
         data-theme={this.state.theme}
         data-menu={this.state.showSkins ? '' : undefined}
@@ -1064,7 +1116,9 @@ export default class Picker extends Component {
         <div ref={this.refs.scroll} class="scroll flex-grow padding-lr">
           <div
             style={{
-              width: this.props.perLine * this.props.emojiButtonSize,
+              width: this.props.dynamicWidth
+                ? '100%'
+                : this.props.perLine * this.props.emojiButtonSize,
               height: '100%',
             }}
           >

packages/emoji-mart/src/components/Picker/PickerProps.ts
@@ -2,6 +2,9 @@ export default {
   autoFocus: {
     value: false,
   },
+  dynamicWidth: {
+    value: false,
+  },
   emojiButtonColors: {
     value: null,
   },

packages/emoji-mart/src/components/Picker/PickerStyles.scss
@@ -451,6 +451,10 @@ button {
 .skin-tone-5 { background-color: #a46134 }
 .skin-tone-6 { background-color: #5d4437 }
 
+[data-index] {
+  justify-content: space-between;
+}
+
 [data-emoji-set="twitter"] {
   .skin-tone:after {
     border-color: rgba(0,0,0, .5);

commit 9302f957f24a30b4a777bac06cd4dca0d2a1b7b0

Fix picker width changing when hovering emojis Regression since 03960bd Fixes #762

packages/emoji-mart/src/components/Picker/Picker.tsx
@@ -1099,10 +1099,17 @@ export default class Picker extends Component {
   }
 
   render() {
+    const lineWidth = this.props.perLine * this.props.emojiButtonSize
+
     return (
       <section
         id="root"
         class="flex flex-column"
+        style={{
+          width: this.props.dynamicWidth
+            ? '100%'
+            : `calc(${lineWidth}px + (var(--padding) + var(--sidebar-width)))`,
+        }}
         data-emoji-set={this.props.set}
         data-theme={this.state.theme}
         data-menu={this.state.showSkins ? '' : undefined}
@@ -1116,9 +1123,7 @@ export default class Picker extends Component {
         <div ref={this.refs.scroll} class="scroll flex-grow padding-lr">
           <div
             style={{
-              width: this.props.dynamicWidth
-                ? '100%'
-                : this.props.perLine * this.props.emojiButtonSize,
+              width: this.props.dynamicWidth ? '100%' : lineWidth,
               height: '100%',
             }}
           >